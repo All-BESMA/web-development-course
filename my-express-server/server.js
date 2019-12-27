2030eec2edadcc64bc0209850f82b9ff700926a2
Eduardo Henrique <eduardohmrodrigues@gmail.com>
2019-12-27 16:58:05 -0300

    Add a project sample using express

---
+++ my-express-server/server.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+const express = require('express');
+const app = express();
+
+app.get("/", function(req, res){
+  res.send("<h1>Hello, world!</h1>");
+});
+
+app.get("/contact", function(req, res){
+  res.send("Contact me at eduardo@gmail.com");
+});
+
+app.get("/about", function(req, res){
+  res.send("I'm a developer");
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
