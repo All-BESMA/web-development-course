336acba958fd43683c74079a88b227d66c846392
Eduardo Henrique <eduardohmrodrigues@gmail.com>
2020-01-25 21:09:41 -0300

    Add EJS course module code

---
+++ todolist-v1/app.js
@@ -0,0 +1,43 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js")
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+var items = [];
+var workItems = [];
+
+app.get("/", function(req, res){
+  let day = date.getDate();
+
+  res.render("list", {listName: day, items: items});
+});
+
+app.post("/", function(req, res){
+  let item = req.body.newItem;
+
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work", function(req, res){
+  res.render("list", {listName: "Work", items: workItems})
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running!");
+});
